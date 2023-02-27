6f6b8e40d8d1c6f180cdf0bee0ea7c6f57542449
Haein Kim <hayni100@gmail.com>
2023-02-26 23:43:21 -0800

    added time blocks and color changing feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,39 @@
 // var currentDay = document.querySelector("#currentDay");
-var currentDay = $('#currentDay');
 
-  var today = moment();
-currentDay.text(today.format('dddd, MMMM Do'));
+const store = window.localStorage;
+const today = moment();
 
-console.log("hello")
+var currentDay = $("#currentDay");
+
+currentDay.text(today.format("dddd, MMMM Do"));
+
+// function that will update block color depending on the current hour//
+function trackTime() {
+  var currentTime = moment().hour();
+// getting elements with class name time-block
+  $(".time-block").each(function () {
+
+//getting id name from each element and turing it into an integer//
+    var scheduleTime = parseInt($(this).attr("id"));
+  
+//changing block color by removing and adding class name//
+    if (scheduleTime < currentTime) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    }
+    if (scheduleTime === currentTime) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    }
+    if (scheduleTime > currentTime) {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+    }
+  });
+
+}
+
+trackTime();
